ab9a372ec58074a4396113d6d528407e7b82a17e
Denisse Solorzano <dsolorzano3@ucol.mx>
2023-10-03 08:28:38 -0600

    Primera Version

---
+++ index.js
@@ -0,0 +1,21 @@
+//Declaraciones de variables en JavaScript moderno
+/*let contador=0;
+contador=8;
+const constante=4*/
+
+
+
+//Alerta en JavaScript
+//alert("Hola esta es una alerta desde JavaScript");
+//console.log("Esto es Javascrpt desde el HTML");
+//console.log(numero);
+//console.log(constante);
+
+//Concatenacion
+let nombre="Denisse";
+let apellido=" Solorzano";
+console.log("Mi nombre es: "+nombre+apellido);
+document.write("Mi nombre es: "+nombre+apellido);
+
+let seleccion =document.querySelector("#seccion1");
+seleccion.innerHTML
